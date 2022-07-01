53b8d808da58f4289ee1f87a16a604298aef89e0
DESKTOP-II8G1G0\Abhishek <abhishek.prasad14@gmail.com>
2022-07-02 03:14:41 +0530

    Added Changes

---
+++ src/main/java/com/ebs/qa/pages/TasksPage.java
@@ -0,0 +1,7 @@
+package com.ebs.qa.pages;
+
+import com.ebs.qa.base.TestBase;
+
+public class TasksPage extends TestBase{
+
+}
---
+++ src/test/java/com/ebs/qa/testcases/LoginPageTest.java
@@ -0,0 +1,54 @@
+package com.ebs.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.ebs.qa.base.TestBase;
+import com.ebs.qa.pages.HomePage;
+import com.ebs.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase{
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest(){
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp(){
+		initialization();
+		loginPage = new LoginPage();	
+	}
+	
+	@Test(priority=1)
+	public void loginPageTitleTest(){
+		String title = loginPage.validateLoginPageTitle();
+		Assert.assertEquals(title, "#1 Free CRM for Any Business: Online Customer Relationship Software");
+	}
+	
+	@Test(priority=2)
+	public void crmLogoImageTest(){
+		boolean flag = loginPage.validateCRMImage();
+		Assert.assertTrue(flag);
+	}
+	
+	@Test(priority=3)
+	public void loginTest(){
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+
+}
